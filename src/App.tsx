8c186f5ac50903573cd0d097423bf07079100d98
yanir <yanir.laty@gmail.com>
2025-03-14 14:14:23 +0200

    AddTaskContainer Added to make App more clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-
+import AddTaskContainer from './components/AddTaskContainer';
 import  TaskList  from './components/TaskList';
 import TaskFunctions from './components/Functions';
+
 
 const App = () => {
   const { tasks, newTask, setNewTask, addTask, toggleTask, removeTask } = TaskFunctions();
@@ -9,14 +10,7 @@ const App = () => {
     <div className = "main">
       <h1>To-Do List</h1>
 
-      <input
-        type="text"
-        placeholder="Type your new task here"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-      />
-      
-      <button onClick={addTask}>Add Task</button>
+      <AddTaskContainer newTask={newTask} setNewTask={setNewTask} addTask={addTask} />
 
       <TaskList tasks={tasks} toggleTask={toggleTask} removeTask={removeTask} />
     </div>
---
+++ src/components/AddTaskContainer.tsx
@@ -0,0 +1,26 @@
+import TaskFunctions from './Functions';
+import {Task} from './TaskList';
+
+interface TaskProps{
+  
+  newTask: string
+  setNewTask: (task: string) => void;
+  addTask: () => void;
+}
+
+const AddTaskContainer = ({ newTask, setNewTask, addTask } : TaskProps) => {
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Type your new task here"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+            />         
+            <button onClick={addTask}>Add Task</button>
+        </div>
+      
+    );
+}
+
+export default AddTaskContainer;
